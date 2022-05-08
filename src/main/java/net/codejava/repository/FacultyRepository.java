bacd51e1ee29a47aeda1425094bc0a05bfabff5f
Mohana Balaji <97578960+mohanabalaji@users.noreply.github.com>
2022-05-08 13:41:38 -0700

    Initial commit

---
+++ src/main/java/net/codejava/repository/FacultyRepository.java
@@ -0,0 +1,17 @@
+package net.codejava.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import net.codejava.entity.Faculty;
+import net.codejava.entity.Student;
+
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+	
+	@Query(value = "select * from faculty s where s.id like %:keyword% or s.name like %:keyword% or s.email like %:keyword% or s.post like %:keyword%", nativeQuery = true)
+	 List<Faculty> findByKeyword(@Param("keyword") String keyword);
+
+}
